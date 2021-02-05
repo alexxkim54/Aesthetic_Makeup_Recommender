3e2944b2f4ccbe0f544e1384eb706a8aee01d64d
jul290 <jul290@ucsd.edu>
2021-02-05 11:16:12 -0800

    test argument is now working for the checkpoint

--- run.py
+++ run.py
@@ -12,7 +12,9 @@ import pickle
 
 sys.path.insert(0, 'src')
 from data.etl import scrape_item_data, scrape_review_data
-
+from features.collab_filter_feats import build_features
+from models.collab_filter_model import collab_model_build
+from evaluation.collab_filter_evaluation import auc_eval
 
 def main(targets):
     '''
@@ -35,7 +37,7 @@ def main(targets):
     if 'test' in targets:
         targets += ['features', 'model', 'accuracy']
         with open('config/test-params.json') as fh:
-            data_cfg = json.load(fh)['testdir']
+            data_cfg = json.load(fh)['inputdir']
 
 
 
@@ -54,17 +56,24 @@ def main(targets):
         
     # create and save the features
     if 'features' in targets:
-
-        pass            
+        (interactions, weights) = build_features(data_cfg)
+                    
         
     
     # load the model using the saved features.
     if 'model' in targets:
-        pass
+        with open('config/model-params.json') as fh:
+            model_cfg = json.load(fh)
+            num_threads = model_cfg['NUM_THREADS']
+            num_components = model_cfg['NUM_COMPONENTS']
+            num_epochs = model_cfg['NUM_EPOCHS']
+            item_alpha = model_cfg['ITEM_ALPHA']
+        collab_filter_model = collab_model_build(interactions, num_threads,
+            num_components, num_epochs, item_alpha)
     
     # Get accuracy of the model
     if 'accuracy' in targets:
-        pass
+        auc_eval(collab_filter_model, interactions, num_threads)
         
         
     
